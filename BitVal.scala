224a44038621999b4664a05ea9ea29ea315cd802
Sruthi Santhosh <35186002+sruthisanthosh@users.noreply.github.com>
2020-01-15 23:46:53 +0530

    Add files via upload

--- BitVal.scala
+++ BitVal.scala
@@ -2,15 +2,38 @@ package BitVal
 
 import chisel3._
 
-class BitVal extends Module{
+abstract class Params{
+	val width: Int 
+}
+
+class actualparams extends Params{
+	width = 16
+}
+
+class BitVal(c: Params) extends Module{
 	val io = IO(new Bundle{
-	val a = Input(UInt(16.W))
-	val b = Input(UInt(16.W))
-	val High = Output(UInt(16.W)
-    val Low = Output(UInt(16.W))
+	val a = Input(UInt(c.width.W))
+	val b = Input(UInt(c.width.W)) 
+	val High = Output(UInt(c.width.W)
+    val Low = Output(UInt(c.width.W)) 
     })
+
+    /*val mybitv = Module(new bitv)
+    mybitv.io.input := io.a & io.b   // assigning a function as an input which in an io in mybitv class 
+    io.Low := mybitv.io.output*/     //assigning the output to the Low output of the first class
     val mul= io.a * io.b
-    val High := mul(31,16)
-    val Low := mul(15,0)
+    val High := mul(2*c.width-1,c.width)
+    val Low := mul(c.width-1,0)
 
 }
+
+
+class bitv extends Module{
+	val io = IO(new Bundle{
+		val input = Input(UInt(16.W))
+		val output = Output(UInt(16.W))
+	})
+	val b = Module(new Bitval(actualparams))
+}
+
+
